25bdd481a5131b94f67f49faefcaf85b94fbdf8a
oleg <o1egov@mail.ru>
2025-10-27 18:15:52 +0700

    создание флота

--- alien.py
+++ alien.py
@@ -10,7 +10,11 @@ class Alien(Sprite):
         self.screen = ai_game.screen
 
         # загрузка изображение пришельца и назначение атрибута rect
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('images/alien2.xcf').convert_alpha()
+
+        new_width = 60
+        new_height = 50
+        self.image = pygame.transform.scale(self.image,(new_width,new_height))
         self.rect = self.image.get_rect()
 
         # каждый пришелец появляется в верхнем левом углу
--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """класс для управления ресурсами и поведением игры"""
@@ -19,6 +20,9 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
 
     def run_game(self):
@@ -72,12 +76,42 @@ class AlienInvasion:
             if bullet.rect.bottom < 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """создание флота вторжения"""
+        # создание пришельца и вычисление количества пришельцев в ряду
+        # интервал между пришельцами равен ширине пришельца
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        """ определяем количество рядов"""
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+        # создание флота вторжения
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+
+    def _create_alien(self, alien_number, row_number):
+        """создание пришельца и размещение его в ряду"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
     def _update_screen(self):
         # при каждом проходе цикла перерисовывается экран
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         # отображение последнего прорисованного экрана.
         pygame.display.flip()
 
